cd3c0f4a777638630f2e829803e7f33a92f8f765
jaapjong <J.deJong-18@student.tudelft.nl>
2019-12-05 10:47:01 +0100

    Add solution to day 5 and all input files files

---
+++ src/main/java/Day5/Part5_1.java
@@ -0,0 +1,97 @@
+package Day5;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import java.util.Scanner;
+
+@SuppressWarnings("Duplicates")
+public class Part5_1 {
+
+    public static void main(String[] args) {
+        try{
+            InputStream s = new FileInputStream("src/main/resources/day5.txt");
+            long startTime = System.nanoTime();
+            System.out.println("Answer: " + solution(s));
+            long endTime = System.nanoTime();
+            System.out.println("Solved in " + ((endTime - startTime) / 100000000.0f) + " seconds");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int solution(InputStream input) {
+        int[] data = opcodes(input);
+        int pointer = 0;
+        String parameterMode;
+
+        int result = 0;
+
+        while(true) {
+            // Get operation
+            String opCode = data[pointer] + "";
+            int operation;
+            if (opCode.length() == 1) {
+                operation = data[pointer];
+                parameterMode = "";
+            } else {
+                operation = Integer.parseInt(opCode.substring(opCode.length() - 2));
+                parameterMode = opCode.substring(0, opCode.length() - 2);
+            }
+
+
+            switch (operation) {
+                case 1: // SUM
+                    data[data[pointer + 3]] = getParameter(parameterMode, data, 1, pointer) + getParameter(parameterMode, data, 2, pointer);
+                    pointer += 4;
+                    break;
+                case 2: // MULTIPLICATION
+                    data[data[pointer + 3]] = getParameter(parameterMode, data, 1, pointer) * getParameter(parameterMode, data, 2, pointer);
+                    pointer += 4;
+                    break;
+                case 3: // INPUT
+                    data[data[pointer + 1]] = 1;
+                    pointer += 2;
+                    break;
+                case 4: // OUTPUT
+                    result = getParameter(parameterMode, data, 1, pointer);
+                    pointer += 2;
+                    break;
+                case 99:
+                    return result;
+                default:
+                    System.out.println("Unknown opcode");
+                    return -1;
+            }
+        }
+    }
+
+    private static int getParameter(String parameterMode, int[] data, int parameter, int pointer) {
+        int mode = 0;
+        if (parameter <= parameterMode.length()) {
+            mode = Integer.parseInt(parameterMode.charAt(parameterMode.length() - parameter) + "");
+        }
+        if (mode == 0) {
+            return data[data[pointer + parameter]];
+        } else {
+            return data[pointer + parameter];
+        }
+    }
+
+    private static int[] opcodes(InputStream in) {
+        Scanner sc = new Scanner(in);
+        StringBuilder sb = new StringBuilder();
+        while(sc.hasNextLine()) {
+            sb.append(sc.nextLine());
+        }
+        sc.close();
+        String inputStr = sb.toString();
+        String[] opcodeStr = inputStr.split(",");
+        int[] opcodes = new int[opcodeStr.length];
+        for (int i = 0; i < opcodeStr.length; i++) {
+            opcodes[i] = Integer.parseInt(opcodeStr[i]);
+        }
+        return opcodes;
+    }
+}
