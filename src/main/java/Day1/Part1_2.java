185d5ac36392cd62c222b758778de775e7caff25
jaapjong <J.deJong-18@student.tudelft.nl>
2019-12-05 10:42:13 +0100

    Add Solution for Day1

---
+++ src/main/java/Day1/Part1_2.java
@@ -0,0 +1,40 @@
+package Day1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Part1_2 {
+    public static void main(String[] args) {
+        try{
+            InputStream s = new FileInputStream("src/main/resources/day1.txt");
+            long startTime = System.nanoTime();
+            System.out.println(solution(s));
+            long endTime = System.nanoTime();
+            System.out.println("Solved in " + ((endTime - startTime) / 100000000.f) + " seconds");
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static String solution(InputStream s) {
+        long totalMass = 0;
+        Scanner sc = new Scanner(s);
+        while(sc.hasNextLine()) {
+            String line = sc.nextLine();
+            int input = Integer.parseInt(line);
+            totalMass += fuelRequired(input);
+        }
+
+        sc.close();
+        return "" + totalMass;
+    }
+
+    public static long fuelRequired(long mass) {
+        if (Math.floor(mass / 3.0) - 2 <= 0) return 0;
+        long fuel = (int)Math.floor(mass / 3.0) - 2;
+        return fuel + fuelRequired(fuel);
+    }
+}
